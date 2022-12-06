8a62811e15ce94c7cc1973dab350eeeaf45579f7
Benson Njuguna <njugunab655@gmail.com>
2022-12-06 10:19:52 +0300

    Added a handleClick function

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,16 +1,21 @@
 import memeSample from "../Images/memeimg.png"
 import memesData from "../memesData"
 
-console.log(memesData)
+function handleClick() {
+    const memesArr = memesData.data.memes
+    const randomIndex = Math.floor(Math.random() * memesArr.length)
+    let randomUrl = memesArr[randomIndex]
+    console.log(randomUrl)
+}
 
 export default function MainContent() {
     return (
         <div className="outer-main-container" >
-            <form className="main-content-container" >
+            <div className="main-content-container" >
                 <input type="text" id="top" placeholder="Message Top" className="top inputs" />
                 <input type="text" id="bottom" placeholder="Message Bottom" className="bottom inputs" />
-                <button className="newMemeBtn" >Get a New Meme Image</button>
-            </form>
+                <button className="newMemeBtn" onClick={handleClick} >Get a New Meme Image</button>
+            </div>
             <div className="preview" >
                 <img src={memeSample} className="memeSample" />
             </div>
