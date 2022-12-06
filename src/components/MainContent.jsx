54a3f464e03b99925aa7cbeeafbcb403dad42610
Benson Njuguna <njugunab655@gmail.com>
2022-12-06 08:33:03 +0300

    Created and imported memesData

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,15 +1,16 @@
 import memeSample from "../Images/memeimg.png"
+import memesData from "../memesData"
+
+console.log(memesData)
 
 export default function MainContent() {
     return (
         <div className="outer-main-container" >
-            <div className="main-content-container" >
-                <div className="inputs-container" >
-                    <input type="text" id="top" placeholder="Message Top" className="top inputs" />
-                    <input type="text" id="bottom" placeholder="Message Bottom" className="bottom inputs" />
-                </div>
+            <form className="main-content-container" >
+                <input type="text" id="top" placeholder="Message Top" className="top inputs" />
+                <input type="text" id="bottom" placeholder="Message Bottom" className="bottom inputs" />
                 <button className="newMemeBtn" >Get a New Meme Image</button>
-            </div>
+            </form>
             <div className="preview" >
                 <img src={memeSample} className="memeSample" />
             </div>
