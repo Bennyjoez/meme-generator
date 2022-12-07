91b799eb4d052e4e0eb19e8b4cf67a04cc5a1c29
Benson Njuguna <njugunab655@gmail.com>
2022-12-07 06:10:13 +0300

    Get new images on button click

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,14 +1,21 @@
-import memeSample from "../Images/memeimg.png"
+import React from "react"
 import memesData from "../memesData"
 
-function handleClick() {
-    const memesArr = memesData.data.memes
-    const randomIndex = Math.floor(Math.random() * memesArr.length)
-    let randomUrl = memesArr[randomIndex]
-    console.log(randomUrl)
-}
+
+
+
 
 export default function MainContent() {
+    // use state
+    let [url, setUrl] = React.useState()
+
+    function handleClick() {
+        const memesArr = memesData.data.memes
+        const randomIndex = Math.floor(Math.random() * memesArr.length)
+        let randomUrl = memesArr[randomIndex].url
+        setUrl(prevUrl => randomUrl)
+    }
+
     return (
         <div className="outer-main-container" >
             <div className="main-content-container" >
@@ -17,7 +24,7 @@ export default function MainContent() {
                 <button className="newMemeBtn" onClick={handleClick} >Get a New Meme Image</button>
             </div>
             <div className="preview" >
-                <img src={memeSample} className="memeSample" />
+                <img src={url} className="memeSample" />
             </div>
         </div>
     )
